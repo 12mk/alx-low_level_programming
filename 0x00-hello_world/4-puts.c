9b4dc3e47cb4cf842ed6d14fbd3179def9e6eeb7
12mk <mekdimtessema29@gmail.com>
2023-03-19 06:33:33 -0700

    0-preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bashi
-gcc puts("Programming is like building a multilingual puzzle)
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts functio
+n
+ * Return: 0(success)
+ */
+int main(void)
+{ 
+puts("\"Programming is like building a multilingual puzz
+le");
+return (0);
+}
+
