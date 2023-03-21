610e3467a51d72e676521af353132b030c9f5fe9
12mk <mekdimtessema29@gmail.com>
2023-03-21 07:49:01 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ lastdigit = n % 10;
 	}
 	else if (lastdigit < 6 && (lastdigit) != 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 	}
 	return (0);
 }
