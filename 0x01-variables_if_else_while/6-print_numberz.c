360fde857d74ddaa6800f20d937be52e328aa83d
12mk <mekdimtessema29@gmail.com>
2023-03-22 00:29:16 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *Return: return 0 value
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar('0' + i);
+	putchar('\n');
+	return (0);
+}
