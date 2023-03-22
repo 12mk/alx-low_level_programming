b418069b17af94bc5ef9e0014ae8d40537012903
12mk <mekdimtessema29@gmail.com>
2023-03-22 00:17:47 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *Return: return o value
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
