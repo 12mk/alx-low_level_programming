67f22ede91f517af84324f172ea84fea2f54902f
12mk <mekdimtessema29@gmail.com>
2023-03-24 06:25:42 -0700

    the second tast

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul - multiplication function
+ *@a: the value of a
+ *@b: the value of second number
+ *Return: return the value of the function
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
