f95d865b5eab89f509600ee18f53568afeb25687
12mk <mekdimtessema29@gmail.com>
2023-03-26 04:49:20 -0700

    this is the sixth task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_line - the function that prints out line
+ *@n: the number returns
+ *Return: return 0 value
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x >= n; x++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
