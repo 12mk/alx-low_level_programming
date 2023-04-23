836e9f9ac278c208f04113a4cac0fa5420d96f1f
12mk <mekdimtessema29@gmail.com>
2023-04-23 06:52:44 -0700

    the dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,15 @@
-#ifndef DOG_H
 #define DOG_H
 
 
-/**
-* struct do - a dog's basic info
-* @name: First member
-* @age: Second member
-* @owner: Third member
-* Description: Longer descriptio
-*/
-	struct do
+	/**
+	 * struct dog - a dog's basic info
+	 * @name: First member
+	 * @age: Second member
+	 * @owner: Third member
+	 *
+	 * Description: Longer description
+	 */
+	struct dog
 	{
 		char *name;
 		float age;
@@ -17,9 +17,9 @@
 	};
 
 
-/**
-* dog_t - typedef for struct dog
-*/
+	/**
+	 * dog_t - typedef for struct dog
+	 */
 	typedef struct dog dog_t;
 
 
@@ -31,4 +31,7 @@
 	int _strlen(char *s);
 
 
-#endif
+	#endif
+
+
+
