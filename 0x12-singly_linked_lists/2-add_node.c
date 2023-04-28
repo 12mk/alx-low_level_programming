6e36ec141a78f9c94dc69cd8d607e2211d2c3381
12mk <mekdimtessema29@gmail.com>
2023-04-27 20:00:10 -0700

    linked 3

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *add_node - the function add node at the start of the ll
+ *@head: double pointer of ll
+ *@str: string to add
+ *Return: return the address
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *head_new;
+	unsigned int leng = 0;
+
+	while (str[leng])
+		 leng++;
+
+	 head_new = malloc(sizeof(list_t));
+	 if (!head_new)
+		 return (null);
+
+	 head_new->str = strdup(str);
+	 head_new->leng = leng;
+	 head_new->next = (*head);
+	 (*head) =  new;
+
+	 return (*head);
+}
